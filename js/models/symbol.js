e52ed6ea365c491a74821a0275a44e3a79418f88
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-29 02:20:34 +0300

    Добавлено представление символа, функционал установки и обновления позиции.

--- js/models/symbol.js
+++ js/models/symbol.js
@@ -1,6 +1,28 @@
 export default class Symbol {
 	constructor (type, view) {
+		this._x = 0;
+		this._y = 0;
 		this.type = type;
 		this.view = view;
+	}
+
+	get x(){
+		return this._x;
+	}
+	get y(){
+		return this._y;
+	}
+	set x(v){
+		this._x = v;
+		this.updatePosition();
+	}
+	set y(v){
+		this._y = v;
+		this.updatePosition();
+	}
+
+	updatePosition(){
+		this.view.x = this._x;
+		this.view.y = this._y;
 	}
 }
