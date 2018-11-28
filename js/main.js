0a50be5b4449ddc7c13cf3c59f8072b045a8c9b3
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-29 02:16:16 +0300

    Изменен размер монитора слота.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import config from './config.js'
 import Game from './services/game.js'
 import ResourceLoader from './services/resourceLoader.js'
 
-const app = new PIXI.Application({width: 256, height: 256});
+const app = new PIXI.Application({width: 500, height: 300});
 document.body.appendChild(app.view);
 
 let game = new Game(app);
