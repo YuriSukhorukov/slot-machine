f7535726c8b72ff5b1d65a3876e4e3d88a85d483
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-28 11:10:35 +0300

    Контейнер ресурсов.

--- js/services/resources.js
+++ js/services/resources.js
@@ -1,9 +1,15 @@
 export default class Resources {
 	constructor (patch) {
 		this._patch = patch;
+		this._storage = [];
 	}
 
 	load () {
 		console.log(`loading from ${this._patch}`);
+		this._storage = [1,2,3];
+	}
+
+	get storage() {
+		return this._storage;
 	}
 }
