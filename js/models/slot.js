b255415595f7dc94ccb5337474a76952cdcb8dfc
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-29 02:21:46 +0300

    Функционал прокрутки катушек.

--- js/models/slot.js
+++ js/models/slot.js
@@ -1,5 +1,12 @@
 export default class Slot {
 	constructor(reels){
 		this.reels = reels;
+		console.log(this.reels);
+	}
+	twist(){
+		for(let i = 0; i < this.reels.length; i++){
+			this.reels[i].y += 1;
+			console.log(this.reels[i].symbols[1].y)
+		}
 	}
 }
