3d45209e0369d887d664b399e7ef86eb9c7a138a
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-29 00:24:39 +0300

    Символ инициализируется типом и представлением, которое PIXI спрайт.

--- js/models/symbol.js
+++ js/models/symbol.js
@@ -1,12 +1,6 @@
 export default class Symbol {
-	constructor (type, width, height, sprite) {
-		this._type = type;
-		this._width = width;
-		this._height = height;
-		this._sprite = sprite;
-	}
-
-	get type(){
-		return this._type;
+	constructor (type, view) {
+		this.type = type;
+		this.view = view;
 	}
 }
