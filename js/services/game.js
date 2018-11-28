a282e8680753bf1d540c776c2f075b2cb20cf732
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-29 00:29:02 +0300

    Логика инициализации игровых объектов.

--- js/services/game.js
+++ js/services/game.js
@@ -1,19 +1,38 @@
+import Symbol from './../models/symbol.js'
+import Reel from './../models/reel.js'
 import Slot from './../models/slot.js'
 
 export default class Game {
+	constructor(app){
+		this.app = app;
+
+		this.sprites;
+		this.audio;
+		this.slot;
+	}
 	initResources(resources) {
 		return new Promise((resolve, reject)=>{
-			console.log(resources.sprites)
-			console.log(resources.audio)
-			console.log('game initialized')
+			this.sprites = resources.sprites;
+			this.audio = resources.audio;
 			resolve();
 		})
 	}
 	initObjects(){
-		this._slot = new Slot();
-		console.log(this._slot);
+		let symbols = [];
+		for(let i = 0; i < this.sprites.length; i++){
+			symbols[i] = new Symbol('coffee_cup', this.sprites[i]);
+		}
+
+		let reels = [];
+		const REELS_AMOUNT = 3;
+		for(let i = 0; i < REELS_AMOUNT; i++){
+			reels[i] = new Reel(symbols);
+		}
+
+		this.slot = new Slot(reels);
 
 		return new Promise((resolve, reject)=>{
+			console.log('objects initialized', this.slot)
 			resolve();
 		})
 	}
