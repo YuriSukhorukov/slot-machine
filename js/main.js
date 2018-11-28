96043ea41b5c2703063c6b1489d150c4a745cf6f
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-28 12:27:34 +0300

    Фабричный метод создания символов.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 import config from './config.js'
 import ResourceLoader from './services/resourceLoader.js'
+import Symbol from './models/symbol.js'
 
 
-let sprites = []
 
 let resourceLoader = new ResourceLoader();
 
-resourceLoader.load(config.sprites.patch).then(spr => sprites = spr).catch(()=>{})
+resourceLoader.load(config.sprites.patch).then(sprites => createSymbols(sprites)).catch(()=>{})
+
+
+function createSymbols(sprites){
+	let symbols = [];
+	symbols.push(new Symbol('cofee', 100, 100, sprites[0]));
+	console.log(symbols);
+}
 
 
 const app = new PIXI.Application({width: 256, height: 256});
