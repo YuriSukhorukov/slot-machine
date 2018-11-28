2af3ba97d364a7bd17217044ea4432068c73ee8e
yuri-spb <yuri.sukhorukov.spb@gmail.com>
2018-11-29 00:28:04 +0300

    Сервис игры инициализируется экземпляром приложения PIXI.

--- js/main.js
+++ js/main.js
@@ -2,21 +2,22 @@ import config from './config.js'
 import Game from './services/game.js'
 import ResourceLoader from './services/resourceLoader.js'
 
-let game = new Game();
-let resourceLoader = new ResourceLoader();
+const app = new PIXI.Application({width: 256, height: 256});
+document.body.appendChild(app.view);
 
+let game = new Game(app);
+let resourceLoader = new ResourceLoader();
 
 resourceLoader.load(config)
 .then((resources)=>{
-	game.initResources(resources);
+	console.log('resources loaded');
+	return game.initResources(resources);
 })
 .then(()=>{
-	game.initObjects();
+	console.log('game resources initialized');
+	return game.initObjects();
 })
 .then(()=>{
+	console.log('game objects initialized');
 	game.start();
 })
-
-
-const app = new PIXI.Application({width: 256, height: 256});
-document.body.appendChild(app.view);
